559859d7046ba39f97882091ee8c2ab579d11d27
alvin <alvin@DESKTOP-AE8PM2K>
2023-05-03 14:00:01 +0530

    till props

---
+++ src/Person.js
@@ -0,0 +1,13 @@
+function Person(props){
+    return(
+        <>
+            <h1>
+                Name:{props.name}
+            </h1>
+            <h2>
+                Age:{props.age}
+            </h2>
+        </>
+    )
+}
+export default Person;
---
+++ src/Test.js
@@ -0,0 +1,9 @@
+import Person from './Person'
+function Test(){
+    return(
+        <>
+            <Person name={"John"} age={10}/>
+        </>
+    )
+}
+export default Test;
